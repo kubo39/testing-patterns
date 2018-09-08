2163104119782dcaf1ad9d9c1129031b25804444
Hiroki Noda <kubo39@gmail.com>
2018-09-08 18:35:09 +0900

    do not need class

--- ParameterizedTest/source/app.d
+++ ParameterizedTest/source/app.d
@@ -1,48 +1,47 @@
 import std.format;
 
-class DiffTests
-{
 private:
-    struct TestCase(T)
-    {
-        T expected;
-        T actual;
-    }
 
-    static void assertEq(T)(uint line, TestCase!T testCase)
-    {
-        assert(testCase.expected == testCase.actual,
-               format("failed at line %d: expected is %s, but actual %s",
-                      line, testCase.expected, testCase.actual));
-    }
+struct TestCase(T)
+{
+    T expected;
+    T actual;
+}
+
+void assertEq(T)(uint line, TestCase!T testCase)
+{
+    assert(testCase.expected == testCase.actual,
+           format("failed at line %d: expected is %s, but actual %s",
+                  line, testCase.expected, testCase.actual));
+}
 
 public:
-    static void testDiff()
+
+void testDiff()
+{
+    TestCase!string[uint] testCases1 = [
+        __LINE__: TestCase!string(
+            "I'm not changed.",
+            "I'm changed."
+            )];
+
+    TestCase!int[uint] testCases2 = [
+        __LINE__: TestCase!int(2, 3)];
+
+    foreach (line, testCase; testCases1)
     {
-        TestCase!string[uint] testCases1 = [
-            __LINE__: TestCase!string(
-                "I'm not changed.",
-                "I'm changed."
-                )];
+        assertEq!string(line, testCase);
+    }
 
-        TestCase!int[uint] testCases2 = [
-            __LINE__: TestCase!int(2, 3)];
-
-        foreach (line, testCase; testCases1)
-        {
-            assertEq!string(line, testCase);
-        }
-
-        foreach (line, testCase; testCases2)
-        {
-            assertEq!int(line, testCase);
-        }
+    foreach (line, testCase; testCases2)
+    {
+        assertEq!int(line, testCase);
     }
 }
 
 unittest
 {
-    DiffTests.testDiff();
+    testDiff();
 }
 
 void main(){}
